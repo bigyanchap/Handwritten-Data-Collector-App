9f70d798715e362ce53d11e26932ec7b4a535054
Bigyan Chapagain <bigyanchapagain@gmail.com>
2021-08-26 16:56:35 +0545

    Initial Commit

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+using static WebApplication1.Models.Enums;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        IWebHostEnvironment host;
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            host = environment;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            HttpContext.Session.SetString("SessionCount","0");
+            return View();
+        }
+
+        public IActionResult InsertDataPage()
+        {
+            int SessionCount = Convert.ToInt32(HttpContext.Session.GetString("SessionCount"));
+            SessionCount++;
+            if (SessionCount <= _class.classList.Count)
+            {
+                _class _class_ = new _class();
+                _class_.classId = SessionCount;
+                _class_.Name = _class.classList.Where(x => x.classId == _class_.classId).FirstOrDefault().Name;
+                _class_.Description = _class.classList.Where(x => x.classId == _class_.classId).FirstOrDefault().Description;
+
+                HttpContext.Session.SetString("SessionCount", SessionCount.ToString());
+                return View(_class_);
+            }
+            else
+            {
+                return RedirectToAction("EndPage");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertData()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+                var classId = Convert.ToInt32(file.Name);
+                _class _class_ = new _class();
+                _class_.Name = _class.classList.Where(x => x.classId == classId).FirstOrDefault().Name;
+                
+                string folderName = _class_.Name;
+                if (file == null) return BadRequest("No file found");
+                string uploadFolderPath = Path.Combine(host.ContentRootPath, "Uploads\\"+folderName+"\\");
+
+                if (!Directory.Exists(uploadFolderPath))
+                {
+                    Directory.CreateDirectory(uploadFolderPath);
+                }
+
+
+                var fileName = string.Empty;
+                if (file != null)
+                {
+                    
+                    fileName = Guid.NewGuid().ToString() + ".png";
+                    var filepath = Path.Combine(uploadFolderPath, fileName);
+                    using (var stream = new FileStream(filepath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(stream);
+                    }
+                   
+                }
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Something went wrong.");
+            }
+        }
+        public IActionResult EndPage()
+        {
+            HttpContext.Session.Remove("SessionCount");
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ WebApplication1/Models/Enums.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Enums
+    {
+        
+        public class _class
+        {
+            public int classId { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public static List<_class> classList
+            {
+                get
+                {
+                    return new List<_class>
+                    {
+                        new _class{classId=1,Name="bra",Description="ब्र"},
+                        new _class{classId=2,Name="tta",Description="त्त"},
+                        new _class{classId=3,Name="mra",Description="म्र"},
+                        new _class{classId=4,Name="mri",Description="मृ"},
+                        new _class{classId=5,Name="dwa",Description="द्व"},
+                        new _class{classId=6,Name="hya",Description="ह्य"},
+                        new _class{classId=7,Name="hra",Description="ह्र"},
+                        new _class{classId=8,Name="gri",Description="गृ"},
+                        new _class{classId=9,Name="rhi",Description="ॠ"},
+                        new _class{classId=10,Name="kra",Description="क्र"},
+                        new _class{classId=11,Name="pra",Description="प्र"},
+                        new _class{classId=12,Name="dya",Description="द्य"},
+                        new _class{classId=13,Name="dda",Description="दृ"},
+                        new _class{classId=14,Name="hri",Description="हृ"},
+                        new _class{classId=15,Name="traw",Description="ट्र"},
+                        new _class{classId=16,Name="tra",Description="त्र"},
+                        new _class{classId=17,Name="dra",Description="द्र"},
+                        new _class{classId=18,Name="dma",Description="द्म"},
+                        new _class{classId=19,Name="shra",Description="श्र"},
+                        new _class{classId=20,Name="pri",Description="पृ"}
+                    };
+                }
+            }
+        }
+    }
+}
